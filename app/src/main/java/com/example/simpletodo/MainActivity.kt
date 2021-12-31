844af42ba0441723f1f56a58ae0c8a409c25c5a8
Michael Romero <michaelromero128@gmail.com>
2021-12-30 21:50:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.simpletodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+
+    var listOfTasks = mutableListOf<String>()
+
+    lateinit var adapter: TaskItemAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val onLongClickListener = object: TaskItemAdapter.OnLongClickListener {
+            override fun onItemLongClicked(position: Int) {
+                listOfTasks.removeAt(position)
+                adapter.notifyDataSetChanged()
+                saveItems()
+
+            }
+
+        }
+        val editTextView = findViewById<EditText>(R.id.addTaskField)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            listOfTasks.add(editTextView.text.toString())
+            adapter.notifyItemInserted(listOfTasks.size -1)
+            editTextView.setText("")
+            saveItems()
+
+        }
+        loadItems()
+
+
+
+
+        val recyclerView =findViewById<RecyclerView>(R.id.recyclerView)
+        adapter = TaskItemAdapter(listOfTasks, onLongClickListener)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+
+    }
+
+    fun getDataFile(): File{
+        return File(filesDir, "data.txt")
+    }
+    fun saveItems(){
+        try{
+            FileUtils.writeLines(getDataFile(),listOfTasks)
+        }catch(ioException :IOException){
+            ioException.printStackTrace()
+        }
+
+    }
+    fun loadItems(){
+        try{
+            listOfTasks = FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        }catch(ioException: IOException){
+            FileUtils.touch(getDataFile())
+        }
+    }
+
+}
